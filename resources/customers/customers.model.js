fe73739f3701f90226894b49446138f16730eb4d
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-05-14 20:10:09 +0100

    change model customers

--- resources/customers/customers.model.js
+++ resources/customers/customers.model.js
@@ -21,7 +21,6 @@ let customerSchema = new mongoose.Schema({
     },
     photoURL: {
         type: String,
-        match: 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)' 
     }    
 });
 
