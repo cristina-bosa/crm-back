0f057d4bf3bbf6417435628d0a042588774dbac8
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-05-14 19:45:00 +0100

    fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const cors = require ('cors')
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const app = express();
 
-app.get('/customers');
+
+//evita que el bodu llegue vacio
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL);
+
+const customersRouter = require('./resources/customers/customers.router');
+
+app.use('/customers',customersRouter);
+
 
 app.listen(3000, (error)=>{
     if(error){
